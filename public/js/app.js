30d15f9692875a6f0c05e751d2d39124805c739b
LilliamHaro <qualia950@gmail.com>
2018-02-15 00:11:32 -0500

    cambiando alert por modal

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,28 @@
 window.addEventListener('load', function() {
   getNews();
 });
-
+$('#myModal').on('shown.bs.modal', function() {
+  $('#myInput').focus();
+});
 function getNews() {
   // instanciando el objeto XMLHttpRequest para el funcionamiento de ajax
   const articleRequest = new XMLHttpRequest();
 
-  articleRequest.open('GET', `https://swapi.co/api/people/`);
+  articleRequest.open('GET', 'https://swapi.co/api/people/');
   // funciones
   articleRequest.onload = addNews;
   articleRequest.onerror = handleError;
   articleRequest.send();
 }
 // funcion para manejar los errores
-function handleError() { 
+function handleError() {
   console.log('se ha presentado un error');
 }
 
 function addNews() {
   const data = JSON.parse(this.responseText);
   const response = data.results;
-  let a = 0;
+  let centinel = 0;
   response.forEach(function(character) {
     let nameCharacter = character.name;
     let heightCharacter = character.height;
@@ -29,22 +31,28 @@ function addNews() {
     let hairCharacter = character.hair_color;
     let massCharacter = character.mass;
     let div = $('<div>');
-    let href = images[a];
-    let img = $('<img src="' + href + '" data-mass="' + massCharacter + '" data-hair="' + hairCharacter + '" data-name ="' + nameCharacter + '" data-heigth ="' + heightCharacter + '" data-birth="' + birthCharacter + '" data-gender="' + genderCharacter + '" alt="">');
+    let href = images[centinel];
+    let img = $('<img data-toggle="modal" data-target=".fade" src="' + href + '" data-mass="' + massCharacter + '" data-hair="' + hairCharacter + '" data-name ="' + nameCharacter + '" data-heigth ="' + heightCharacter + '" data-birth="' + birthCharacter + '" data-gender="' + genderCharacter + '" alt="">');
     div.append(img);
     // div.textContent = nameCharacter;
     $('#response-container').append(div);
-    a++;
+    centinel++;
     // console.log(character);
   });
 
   $('#response-container div img').on('click', function(event) {
+    $('#body').empty();
     var dataName = 'Nombre: ' + $(this).data('name');
     let dataHeigth = 'Altura: ' + $(this).data('heigth');
     let databirth = 'Año de Nacimiento: ' + $(this).data('birth');
     let datagender = 'Género: ' + $(this).data('gender');
     let dataHair = 'Color de Cabello: ' + $(this).data('hair');
     let dataMass = 'Peso: ' + $(this).data('mass') + ' libras';
-    alert(dataName + '\n' + dataHeigth + '\n' + dataMass + '\n' + databirth + '\n' + datagender + '\n' + dataHair);
+    let dataCharacter = '<div>' + dataName + '</div>' + '<div>' + dataHeigth + '</div>' + '<div>' + dataMass + '</div>' + '<div>' + databirth + '</div>' + '<div>' + datagender + '</div>' + dataHair;
+    $('#body').html(dataCharacter);
+    let image = $(this).clone();
+    let divImg = $('<div>');
+    divImg.append(image);
+    $('#body').append(divImg);
   });
 };
